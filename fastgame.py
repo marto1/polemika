2e8f30118ab6588185db4aea34d7fb976e2a5e57
Martin Gergov <martingergov1@gmail.com>
2016-06-08 00:06:22 +0200

    some cleaning

--- fastgame.py
+++ fastgame.py
@@ -15,33 +15,40 @@ import random
 
 #TODO refactor
 
-pygame.init()
-screen_width, screen_height = (800,600)
-surface = pygame.display.set_mode((screen_width,screen_height))#, HWSURFACE|HWPALETTE, 8) #pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN
-t_surface = pygame.Surface((screen_width,25),pygame.SRCALPHA)
-t_lb_surface = pygame.Surface((screen_width,400),pygame.SRCALPHA)
-TABLE_COLOR = (50,50,50,80)
-t_surface.fill(TABLE_COLOR)
-t_lb_surface.fill(TABLE_COLOR)
-pygame.display.set_caption("Fast memory game")
-clock = pygame.time.Clock()
-f = "/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-L.ttf"
-font = pygame.font.Font(f,20)
-big_font = pygame.font.Font(f,24)
-prep_img = lambda x: pygame.transform.scale(
-    pygame.image.load(x),
-    (250, 150))
+#constants
+STDFONT = "/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-L.ttf"
+PROGRESS_TEXT = " "*60+"Time: {0}"
 SELECTED = (128, 0, 100)
 UNSELECTED = (128,128,128)
-PROGRESS_TEXT = " "*60+"Time: {0}"
 DESIRED_FPS = 60.0
 TIME = time(0, 1, 0)
-k = 100
+SCREEN_WIDTH, SCREEN_HEIGHT = (800,600)
+DEFAULT_TABLE_COLOR = (50,50,50,80)
+VICTORY_TABLE_COLOR=(0,180,0,80)
+DEFEAT_TABLE_COLOR = (180,0,0,80)
 time_total_seconds = lambda tm: tm.hour*60*60 + tm.minute*60 + tm.second
 BAR_TICK = 100.0 / time_total_seconds(TIME)
+
+pygame.init()
+
+#global vars
+surface = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+t_surface = pygame.Surface((SCREEN_WIDTH,25),pygame.SRCALPHA)
+t_lb_surface = pygame.Surface((SCREEN_WIDTH,400),pygame.SRCALPHA)
+t_surface.fill(DEFAULT_TABLE_COLOR)
+t_lb_surface.fill(DEFAULT_TABLE_COLOR)
+pygame.display.set_caption("Fast memory game")
+clock = pygame.time.Clock()
+font = pygame.font.Font(STDFONT,20)
+big_font = pygame.font.Font(STDFONT,24)
+progress_percent = 100
 remaining_time = copy(TIME)
 selected_index = 0
 
+
+prep_img = lambda x: pygame.transform.scale(
+    pygame.image.load(x),
+    (250, 150))
 
 def drawText(message,pos,color=(255,255,255)):
     surface.blit(font.render(message,1,color),pos)
@@ -54,7 +61,7 @@ def drawSlot(message, pos, color=(128,128,128), size=(500, 21)):
 
 def drawProgressBar(message, progress):
     """progress from 0 to 100"""
-    total = screen_width-50
+    total = SCREEN_WIDTH-50
     progressw = int((total/100.0)*(100-progress))
     drawSlot("", (20, 420), size=(total, 70))
     drawSlot(message, (20, 420), (0, 128, 233), (total-progressw, 70))
@@ -73,24 +80,26 @@ def draw_slots(words, selected_index, offset, margin):
         multiplier += 1
 
 def draw_game():
-    global k
+    global progress_percent
     global remaining_time
     global selected_index
     w,h = font.size("FPS:        ")
     margin = 35
     surface.blit(pygame.transform.scale(t_surface,(w,h)),
-                 (8,screen_height-30))
+                 (8,SCREEN_HEIGHT-30))
     surface.blit(t_lb_surface,(0,0))
-    drawText("FPS: " + str(int(clock.get_fps())),(10,screen_height-30))
+    drawText("FPS: " + str(int(clock.get_fps())),(10,SCREEN_HEIGHT-30))
     surface.blit(big_font.render("Guess words",0,
-                                 (255,255,255)),(screen_width/2.5,20))
+                                 (255,255,255)),(SCREEN_WIDTH/2.5,20))
     draw_slots(WORDS, selected_index, (30,20), 35)
     if WORDS[selected_index]['pic']:
         surface.blit(
             WORDS[selected_index]['pic'],
-            (screen_width-260, 20+margin))
+            (SCREEN_WIDTH-260, 20+margin))
 
-    drawProgressBar(PROGRESS_TEXT.format(str(remaining_time)), round(k))
+    drawProgressBar(
+        PROGRESS_TEXT.format(str(remaining_time)),
+        round(progress_percent))
     #TODO make progress bar smooth
 
 def game_tick():
@@ -125,8 +134,7 @@ def process_events(): #why polling? can't we use twisted for that?
             elif event.key == pygame.K_RETURN:
                 issuccess = check_success(WORDS)
                 if issuccess == True:
-                    TABLE_COLOR=(0,180,0,80)
-                    t_lb_surface.fill(TABLE_COLOR)
+                    t_lb_surface.fill(VICTORY_TABLE_COLOR)
                     global rem
                     rem.stop()
                     task.deferLater(reactor, 2, reset_game)
@@ -140,12 +148,10 @@ def show_correct(words):
 def countdown_remaining_time():
     global remaining_time
     global rem
-    global TABLE_COLOR
-    global k
+    global progress_percent
     #datetime.time can't do arithmetics, horrible code below
     if not remaining_time.hour and not remaining_time.minute and not remaining_time.second:
-        TABLE_COLOR = (180,0,0,80)
-        t_lb_surface.fill(TABLE_COLOR)
+        t_lb_surface.fill(DEFEAT_TABLE_COLOR)
         rem.stop()
         task.deferLater(reactor, 2, reset_game)
         show_correct(WORDS)
@@ -155,7 +161,7 @@ def countdown_remaining_time():
     hours, remainder = divmod(remaining_time.seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     remaining_time = time(hours, minutes, seconds)
-    k -= BAR_TICK
+    progress_percent -= BAR_TICK
 
 def read_whole_dict(filename):
     with open(filename, "r") as f:
@@ -184,8 +190,7 @@ def reset_game():
     remaining_time = copy(TIME)
     k = 100
     rem.start(1)
-    TABLE_COLOR = (50,50,50,80)
-    t_lb_surface.fill(TABLE_COLOR)
+    t_lb_surface.fill(DEFAULT_TABLE_COLOR)
     total_words = process_dict(data)
     WORDS = random.sample(total_words, 5)
 
