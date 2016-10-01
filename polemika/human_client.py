73f004796b6043c192b557aa147cae2f9304cabc
Martin Gergov <martingergov1@gmail.com>
2016-10-01 22:44:54 +0200

    pep8 human client

--- polemika/human_client.py
+++ polemika/human_client.py
@@ -1,29 +1,21 @@
 # -*- coding: utf-8 -*-
 from twisted.internet.task import LoopingCall
-from twisted.internet import reactor, protocol, task
-from twisted.internet import fdesc, defer, error
+from twisted.internet import reactor
 import pygame
-from datetime import datetime, date
-import pygame,random,math
 from pygame.locals import *
-from datetime import time, tzinfo, timedelta
-from copy import copy
 from dummy_player import DummyAI, reset_state, write_on_connection
 from twisted.internet.endpoints import TCP4ClientEndpoint
 from twisted.internet.endpoints import connectProtocol
-from mechanics import COMMANDS, cmd, GameProtocol, Bunch
-from functools import partial
+from mechanics import cmd, Bunch
 from layout import create_layout
-import random
+from gui import draw_progressbar, draw_slots, draw_players
 
-from gui import draw_inputbox, draw_slot_text
-from gui import draw_progressbar, draw_slots
-from gui import draw_player_correct_bar, draw_players
 
 def to_hms(seconds):
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     return "%d:%02d:%02d" % (h, m, s)
+
 
 def convert_to_dict(words):
     res = []
@@ -33,11 +25,12 @@ def convert_to_dict(words):
         else:
             p = prep_img("pics/black.jpg")
         res.append({
-            "word" :  word[0].decode("utf-8"),
-            "pic"  :  p,
-            "trans":  u"",
-            "guess":  u"",})
+            "word": word[0].decode("utf-8"),
+            "pic": p,
+            "trans": u"",
+            "guess": u""})
     return res
+
 
 class HumanPlayer(DummyAI):
 
@@ -75,8 +68,8 @@ class HumanPlayer(DummyAI):
         BAR_TICK = 100.0 / TIME
         progress_percent = 100
         self.state.progress_percent = 100
-        self.state.guesses = {k : [0,0,0,0,0] for k in guesses.keys()}
-        guesses = {k : [0,0,0,0,0] for k in guesses.keys()}
+        guesses = {k: [0, 0, 0, 0, 0] for k in guesses.keys()}
+        self.state.guesses = guesses
         self.state.block_input = True
 
     def process_guesses(self, data):
@@ -95,23 +88,25 @@ class HumanPlayer(DummyAI):
 
     def process_players(self, data):
         global guesses
-        guesses = {x : [0,0,0,0,0] for x in data[0]}
-        self.state.guesses = {x : [0,0,0,0,0] for x in data[0]}
+        guesses = {x: [0, 0, 0, 0, 0] for x in data[0]}
+        self.state.guesses = guesses
+
 
 #constants
 STDFONT = "/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-L.ttf"
 PROGRESS_TEXT = "Time: {0}"
 SELECTED = (128, 0, 100)
-UNSELECTED = (128,128,128)
+UNSELECTED = (128, 128, 128)
 DESIRED_FPS = 60.0
-SCREEN_WIDTH, SCREEN_HEIGHT = (800,600)
-DEFAULT_TABLE_COLOR = (50,50,50,80)
-VICTORY_TABLE_COLOR=(0,180,0,80)
-DEFEAT_TABLE_COLOR = (180,0,0,80)
+SCREEN_WIDTH,  SCREEN_HEIGHT = (800, 600)
+DEFAULT_TABLE_COLOR = (50, 50, 50, 80)
+VICTORY_TABLE_COLOR = (0, 180, 0, 80)
+DEFEAT_TABLE_COLOR = (180, 0, 0, 80)
 time_total_seconds = lambda tm: tm.hour*60*60 + tm.minute*60 + tm.second
 BAR_TICK = 0
 TOTAL_PERCENT = 100
 PROGRESS_STEP = float(TOTAL_PERCENT) / DESIRED_FPS
+
 
 #global vars
 surface, t_lb_surface = None, None
@@ -119,31 +114,35 @@ clock, small_font, font, big_font = None, None, None, None
 progress_percent, guesses, TIME, remaining_time = None, None, None, None
 layout = None
 
+
 def init_pygame():
     global surface, t_lb_surface
     global clock, small_font, font, big_font, layout
     global progress_percent, guesses, TIME, remaining_time
     pygame.init()
-    surface = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),
+    surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT),
                                       RESIZABLE)
-    t_lb_surface = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT),
+    t_lb_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT),
                                   pygame.SRCALPHA)
     t_lb_surface.fill(DEFAULT_TABLE_COLOR)
     pygame.display.set_caption("Fast memory game")
     clock = pygame.time.Clock()
-    small_font = pygame.font.Font(STDFONT,14)
-    font = pygame.font.Font(STDFONT,22)
-    big_font = pygame.font.Font(STDFONT,28)
+    small_font = pygame.font.Font(STDFONT, 14)
+    font = pygame.font.Font(STDFONT, 22)
+    big_font = pygame.font.Font(STDFONT, 28)
     progress_percent = 100
     guesses = {}
     TIME = -1
     remaining_time = -1
     layout = create_layout(surface, 20, 20)
 
+
 prep_img = lambda x: pygame.image.load(x)
 
-def draw_text(message,pos,color=(255,255,255)):
-    surface.blit(font.render(message,1,color),pos)
+
+def draw_text(message, pos, color=(255, 255, 255)):
+    surface.blit(font.render(message, 1, color), pos)
+
 
 #horrible hacks/wrappers
 def draw_grid_image(s, pos, size, state):
@@ -152,32 +151,36 @@ def draw_grid_image(s, pos, size, state):
             WORDS[state.selected_index]['pic'], size),
         pos)
 
+
 def draw_grid_infobar(s, pos, size):
     draw_text(
         PROGRESS_TEXT.format(str(to_hms(remaining_time))),
         pos)
 
+
 def draw_grid_background(s, pos, size):
-    s.blit(t_lb_surface,pos)
+    s.blit(t_lb_surface, pos)
+
 
 def draw_grid_fps_counter(s, pos, size):
     down = size[1]/2
-    w,h = font.size("FPS:        ")
+    w, h = font.size("FPS:        ")
     draw_text("FPS: " + str(int(clock.get_fps())),
               (pos[0], pos[1]+down))
+
 
 def draw_game(state):
     global layout
     global progress_percent
     w_args = (font, WORDS, state.selected_index, 1, SELECTED)
     p = round(progress_percent)
-    pr_args1 =  (font, p, False, (100,100,100), (0, 128, 233))
-    pr_args2 =  (font, p, True)
+    pr_args1 = (font, p, False, (100, 100, 100), (0, 128, 233))
+    pr_args2 = (font, p, True)
     pl_args = (small_font, guesses)
     layout.put(draw_grid_background, 0, 0, 20, 20)
     layout.put(draw_grid_fps_counter, 0, 18, 1, 1)
     layout.put(draw_slots, 4, 11, 12, 9, *w_args)
-    if len(WORDS) > 0: #at startup there are no words
+    if len(WORDS) > 0:  # at startup there are no words
         if WORDS[state.selected_index]['pic']:
             layout.put(draw_grid_image, 4, 1, 12, 10, state)
     layout.put(draw_grid_infobar, 0, 1, 4, 10)
@@ -188,10 +191,12 @@ def draw_game(state):
     if progress_percent >= PROGRESS_STEP:
         progress_percent -= PROGRESS_STEP
 
+
 def game_tick(state):
     clock.tick(DESIRED_FPS)
     draw_game(state)
     pygame.display.flip()
+
 
 #why polling? can't we use twisted for that?
 def process_events(state, p):
@@ -224,15 +229,19 @@ def process_events(state, p):
                     state.selected_index = len(WORDS) - 1
             elif event.key == pygame.K_RETURN:
                 p.write(cmd.guesses, [x['guess'] for x in WORDS])
-            else: #assume text input
+            else:   # assume text input
                 if len(WORDS) > 0:
                     WORDS[state.selected_index]["guess"] += event.unicode
+
 
 def show_correct(words):
     for word in words:
         word["guess"] += u"  •  " + word["trans"]
 
+
 WORDS = []
+
+
 def start_game_loops(p, state):
     tick = LoopingCall(game_tick, state)
     tick.start(1.0 / DESIRED_FPS)
@@ -241,12 +250,14 @@ def start_game_loops(p, state):
     ev.start(1.0 / 30)
     return p
 
+
 def reset_human_client_state(state):
     state.progress_percent = 100
     state.selected_index = 0
     state.guesses = {}
     state.block_input = False
     return state
+
 
 if __name__ == '__main__':
     init_pygame()
